e6d0e8b30318c3402a42c5ef7baacfd67d08e208
stech” <smitsavaliya321@gmail.com>
2024-01-27 13:05:04 +0530

    ecommerce

--- controller/users/cart.controller.js
+++ controller/users/cart.controller.js
@@ -24,7 +24,6 @@ exports.addCart = async (req, res) => {
 // localhost:8000/api/v1/users/cart/my-cart
 exports.getAllCart = async (req, res) => {
   try {
-    let totalAmountSum = allCartItems.reduce((sum, item) => sum + item.totalAmount, 0);
     let cartProduct = await cartService.getAllCarts({user: req.user._id,isDelete: false});
     let allCartItems = cartProduct.map((e) => ({
       _id:e._id,
